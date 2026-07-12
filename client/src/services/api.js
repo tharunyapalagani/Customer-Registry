b4f8b4ca20d2b6d24ee7a21cd3d439891c3f914c
tharunyapalagani <tharunyapalagani@gmail.com>
2026-07-12 15:30:45 +0530

    Fix API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://customer-registry-lw1u.onrender.com"
+    baseURL: "https://customer-registry-lw1u.onrender.com/api"
 });
 
 export default API;
